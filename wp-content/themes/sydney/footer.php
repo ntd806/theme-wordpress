348a823e96f0ae89254437c1058124128ae1f5f8
dangnghia <nghia.dang@headcapital.com>
2021-02-03 21:09:24 +0700

    add messenger fb chat

--- wp-content/themes/sydney/footer.php
+++ wp-content/themes/sydney/footer.php
@@ -30,6 +30,31 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+        });
+    };
 
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+     attribution="setup_tool"
+     page_id="113081933582348"
+     theme_color="#67b868"
+     logged_in_greeting="Xin chào! Shop có thể giúp gì được cho bạn?"
+     logged_out_greeting="Xin chào! Shop có thể giúp gì được cho bạn?">
+</div>
 </body>
 </html>
